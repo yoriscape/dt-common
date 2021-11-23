886cb9a28544b9b4461ec8c1e9b110ce3aa22859
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-24 00:27:24 +0530

    sm8250-common: Disable ART debug and optimize dexpreopt
     * minimize java debug info
     * use everything filter for dexpreopt (https://github.com/AOSPA/android_vendor_pa/commit/ab5061c425e61bc7d36f7bcf480feea975795545)
     * disable debug configs in art
     * use non-debug dex2oat

--- kona.mk
+++ kona.mk
@@ -180,6 +180,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
